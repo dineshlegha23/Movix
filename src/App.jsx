c3f196cb370043f7c418cb898a1db61270e67c38
Dinesh <dineshlegha23@gmail.com>
2023-12-20 19:19:34 +0530

    Routing & Store implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+import { fetchDataFromApi } from "./utils/api";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Detail";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((store) => store.home);
+
+  useEffect(() => {
+    fetchApiConfig();
+  }, []);
+
+  const fetchApiConfig = async () => {
+    const res = await fetchDataFromApi("/configuration");
+    const url = {
+      backdrop: res.images.secure_base_url + "original",
+      poster: res.images.secure_base_url + "original",
+      profile: res.images.secure_base_url + "original",
+    };
+    dispatch(getApiConfiguration(url));
+    console.log(res);
+  };
+
   return (
-    <>
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
