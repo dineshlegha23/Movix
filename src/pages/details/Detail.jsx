28c2f4186508ab6f57c3c49d7403f64bda1e3419
Dinesh <dineshlegha23@gmail.com>
2023-12-21 21:20:03 +0530

    Added Cast and Official Videos Section

--- src/pages/details/Detail.jsx
+++ src/pages/details/Detail.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+
+import "./style.scss";
+import VideosSection from "./videosSection/VideosSection";
 
 const Detail = () => {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
   return (
-    <div>Detail</div>
-  )
-}
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
+    </div>
+  );
+};
 
-export default Detail
+export default Detail;
